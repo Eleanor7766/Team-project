ba34ebcc252534c5167d85baca7767fe3bb10b9b
Eleanor7766 <220039116@aston.ac.uk>
2024-03-11 14:37:55 +0000

    trs

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ var conn = mysql.createConnection({
     host:"localhost",
     user:"TeamProject17",
     password:"TeamProject17EWAMSHA?",
-    database:"WineShope"
+    database:"WineShop"
 });
 
 conn.connect(function(err){
